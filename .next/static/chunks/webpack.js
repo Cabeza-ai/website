136f451a5657f63ebe918c189f7619c39625d4f4
cabeza-ai <cabeza.ai.info@gmail.com>
2025-07-09 10:39:30 +0530

    chat bot added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "295b3338ffbd2c3b"; }
+/******/ 		__webpack_require__.h = function() { return "478492e3e6a45a6e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
